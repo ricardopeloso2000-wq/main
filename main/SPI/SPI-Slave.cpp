1e7b7185de61665d08075a72977d20ad6d2adc71
ricardopeloso2000-wq <ricardopeloso2000@gmail.com>
2025-12-23 20:31:11 -0300

    Made the init of the Slave class

--- main/SPI/SPI-Slave.cpp
+++ main/SPI/SPI-Slave.cpp
@@ -1,1 +1,100 @@
 #include "SPI-Slave.h"
+
+SPI_Slave& SPI_Slave::VSPI_Instance()
+{
+    static SPI_Slave VSPI_Slave_Isnt(VSPI_HOST);
+    return VSPI_Slave_Isnt;
+}
+
+SPI_Slave& SPI_Slave::HSPI_Instance()
+{
+    static SPI_Slave HSPI_Slave_Isnt(HSPI_HOST);
+    return HSPI_Slave_Isnt;
+}
+
+SPI_Slave::SPI_Slave(spi_host_device_t Id)
+{
+    Slave_Id = Id;
+    if(Id == VSPI_HOST) VSPI_INIT();
+    if(Id == HSPI_HOST) HSPI_INIT();
+}
+
+void SPI_Slave::VSPI_INIT()
+{
+    spi_bus_config_t buscfg;
+    buscfg.mosi_io_num = VSPI_MOSI;
+    buscfg.miso_io_num = VSPI_MISO;
+    buscfg.sclk_io_num = VSPI_CLK;
+    buscfg.quadwp_io_num = -1;
+    buscfg.quadhd_io_num = -1;
+    buscfg.max_transfer_sz = 0;
+   
+    spi_slave_interface_config_t devcfg;
+    devcfg.mode = 1;
+    devcfg.queue_size = 6;
+    devcfg.spics_io_num = VSPI_CS;
+
+    switch(spi_slave_initialize(Slave_Id, &buscfg, &devcfg , SPI_DMA_CH_AUTO))
+    {
+        case ESP_ERR_INVALID_STATE:
+            ESP_LOGE(SPI_Tag , "host already is in use");
+            break;
+        case ESP_ERR_NOT_FOUND:
+            ESP_LOGE(SPI_Tag , "there is no available DMA channel");
+            break;
+        case ESP_ERR_NO_MEM:
+            ESP_LOGE(SPI_Tag , "ESP out of memory");
+            break;
+        default:
+            break;
+    }
+}
+
+void SPI_Slave::HSPI_INIT()
+{
+    spi_bus_config_t buscfg;
+    buscfg.mosi_io_num = HSPI_MOSI;
+    buscfg.miso_io_num = HSPI_MISO;
+    buscfg.sclk_io_num = HSPI_CLK;
+    buscfg.quadwp_io_num = -1;
+    buscfg.quadhd_io_num = -1;
+    buscfg.max_transfer_sz = 0;
+   
+    spi_slave_interface_config_t devcfg;
+    devcfg.mode = 1;
+    devcfg.queue_size = 6;
+    devcfg.spics_io_num = HSPI_CS;
+
+    switch(spi_slave_initialize(Slave_Id, &buscfg, &devcfg , SPI_DMA_CH_AUTO))
+    {
+        case ESP_ERR_INVALID_STATE:
+            ESP_LOGE(SPI_Tag , "host already is in use");
+            break;
+        case ESP_ERR_NOT_FOUND:
+            ESP_LOGE(SPI_Tag , "there is no available DMA channel");
+            break;
+        case ESP_ERR_NO_MEM:
+            ESP_LOGE(SPI_Tag , "ESP out of memory");
+            break;
+        default:
+            break;
+    }
+}
+
+SPI_Slave::~SPI_Slave()
+{
+    spi_slave_free(Slave_Id);
+}
+
+bool SPI_Slave::PutMessageOnTXQueue(uint8_t* TX_buf)
+{
+    if(TX_buf == nullptr) return false;
+    if(RX_queue.size() >= 6)
+    {
+        ESP_LOGI(SPI_Tag , "Queue Full droping last RX_buf");
+        RX_queue.pop();
+    }
+
+    
+
+}   
--- main/SPI/SPI-Slave.h
+++ main/SPI/SPI-Slave.h
@@ -4,6 +4,7 @@
 #include <queue>
 #include "../SmartPointers/DMASmartPointer.h"
 #include "driver/spi_slave.h"
+#include "esp_log.h"
 
 static const char* SPI_Tag = "SPI_slave";
 
@@ -14,19 +15,40 @@ static const char* SPI_Tag = "SPI_slave";
 
 #define HSPI_MOSI 13
 #define HSPI_MISO 12
-#define HSPI_SCLK 14
+#define HSPI_CLK 14
 #define HSPI_CS   15
+
+#define SLAVE_RX_QUEUE_SIZE 6
+#define BUFFSIZE 4096
 
 class SPI_Slave
 {
     public:
-    SPI_Slave& VSPI_Instance();
-    SPI_Slave& HSPI_Instance();
+    static SPI_Slave& VSPI_Instance();
+    static SPI_Slave& HSPI_Instance();
 
     SPI_Slave(SPI_Slave&) = delete;
     SPI_Slave operator=(SPI_Slave&) = delete;
 
+    bool PutMessageOnTXQueue(uint8_t* TX_buf);
+    bool GetMessageOnRXQueue(DMASmartPointer<uint8_t>& smt_ptr);
+
+    void IRAM_ATTR Pre_Callback(spi_transaction_t* trans);
+    void IRAM_ATTR Pos_Callback(spi_transaction_t* trans);
+
     private:
+    SPI_Slave(spi_host_device_t Id);
+    ~SPI_Slave();
+
+    void VSPI_INIT();
+    void HSPI_INIT();
+
+    void Pre_routine();
+    void Pos_routine();
+
+    spi_host_device_t Slave_Id;
+
+    bool SPI_transaction_ongoing = false;
     std::queue<DMASmartPointer<uint8_t>> RX_queue;
 };
 
